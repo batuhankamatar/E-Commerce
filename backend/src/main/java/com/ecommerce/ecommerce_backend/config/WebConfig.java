1f310fc15fab67defeaf8083c4219370f906f591
Batuhan Kamatar <kamatar3@gmail.com>
2026-05-17 14:03:26 +0300

    Add Dockerfile and WebConfig

---
+++ backend/src/main/java/com/ecommerce/ecommerce_backend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://e-commerce-delta-peach-19.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
