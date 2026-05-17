c6aab5bc7f88e3be016e959b3b71e178acaef7eb
Batuhan Kamatar <kamatar3@gmail.com>
2026-05-17 16:59:23 +0300

    debug: add precise startup timestamps to break the loop

--- backend/src/main/java/com/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
+++ backend/src/main/java/com/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
@@ -1,5 +1,6 @@
 package com.ecommerce.ecommerce_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println(">>> [DEBUG-STARTUP] Main Metodu Tetiklendi! Saniye Sayacı Başlıyor...");
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		System.out.println(">>> [DEBUG-STARTUP] Spring Bagimliliklari ve Configler Tamamlandi!");
+	}
 }
